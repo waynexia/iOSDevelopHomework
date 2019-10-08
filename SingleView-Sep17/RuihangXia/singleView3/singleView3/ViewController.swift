d1c6085e3df4cc3134eba0aee3532dc2da9ca2cb
Ruihang Xia <waynestxia@gmail.com>
2019-10-08 16:01:23 +0800

    dismiss and segue

---
+++ SingleView-Sep17/RuihangXia/singleView3/singleView3/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  singleView3
+//
+//  Created by imac on 2019/10/8.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func exitToIndex(segue: UIStoryboardSegue){
+        print("need not to print anything")
+    }
+
+}
+
---
+++ SingleView-Sep17/RuihangXia/singleView3/singleView3/aName.swift
@@ -0,0 +1,35 @@
+//
+//  aName.swift
+//  singleView3
+//
+//  Created by imac on 2019/10/8.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class aName: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func registerBuuttonClick(_ sender :Any){
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
