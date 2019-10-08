3601f54078595a634071bc88ab6a639e4f1d6c2f
forwhfang <945338423@qq.com>
2019-10-08 16:43:11 +0800

    add: wenhao homework4

---
+++ Homework4-Oct8/WenhaoFang/myApp2/myApp2/ResetPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResetPasswordViewController.swift
+//  myApp2
+//
+//  Created by Apple on 2019/10/8.
+//  Copyright © 2019 newbee. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
