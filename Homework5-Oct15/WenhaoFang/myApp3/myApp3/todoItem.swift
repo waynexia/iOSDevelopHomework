21ee04294c95925953280f6d9de692714591b5f0
forwhfang <945338423@qq.com>
2019-10-22 15:10:25 +0800

    add: Wenhao Homework5

---
+++ Homework5-Oct15/WenhaoFang/myApp3/myApp3/todoItem.swift
@@ -0,0 +1,19 @@
+//
+//  todoItem.swift
+//  myApp3
+//
+//  Created by Apple on 2019/10/15.
+//  Copyright © 2019 newbee. All rights reserved.
+//
+
+import Foundation
+
+class todoItem {
+    var title: String?
+    var description: String?
+    
+    init(title: String?, description: String?) {
+        self.title = title
+        self.description = description
+    }
+}
